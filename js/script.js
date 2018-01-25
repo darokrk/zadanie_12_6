3feecc1e10efef7ad8c84ae936b2ec17cde5f1a1
Dariusz Rosiek <daro.krk@interia.pl>
2018-01-25 14:45:29 +0100

    Added script search countries on RESTapi in js/script.js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// variable declaration
+
+var url = 'https://restcountries.eu/rest/v2/name/';
+var countriesList = $('#countries');
+
+// attaching a request for a button
+
+$('#search').click(searchCountries);
+$('#country-name').keypress(searchCountries);
+
+// function implementation onclick
+
+function searchCountries() {
+	var countryName = $('#country-name').val();
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
+		url: url + countryName,
+		method: 'GET',
+		success: showCountriesList
+	});
+}
+
+// show country list function implementation
+
+function showCountriesList(resp) {
+	countriesList.empty();
+	resp.forEach(function(item) {
+		$('<li>')
+		.append($('<h3>').text(item.name))
+		.append($('<p>').text('Capital city: ' + item.capital))
+		.append($('<p>').text('Region: ' + item.region))
+		.append($('<p>').text('Population: ' + item.population))
+		.append($('<p>').text('Area: ' + item.area))
+		.append($('<p>').text('Time-zones: ' + item.timezones))
+		.append($('<div>').prepend('<img src="' + item.flag + '">'))
+		.appendTo(countriesList);
+	});
+}
+
